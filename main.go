99962977b2eb9610d76f5df91881309337d78d56
Noam Wertheim <noam.wertheim@gmail.com>
2022-06-26 13:44:20 +0300

    w

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -15,22 +16,14 @@ func GetCommitsSinceBranch() []string {
 	ctx := context.Background()
 	// git reflog show --no-abbrev --grep-reflog="Created" $(git rev-parse --abbrev-ref HEAD)
 	// create command
-	args := []string{"reflog", "show", "--no-abbrev", "$(git rev-parse --abbrev-ref HEAD)"}
+	//args := []string{"reflog", "show", "--no-abbrev", "$(git rev-parse --abbrev-ref HEAD)"}
+	args := []string{"log", "--format=%H,%dMOISHE"}
 	cmd := exec.CommandContext(ctx, "git", args...)
 	cmd.Dir, _ = os.Getwd()
 	out, err := cmd.Output()
+	splitStr := strings.Split(string(out), "MOISHE")
 
-	log.Println(out, "err", err)
-	// 	dir, _ = filepath.Abs(dir)
-	// 	cmd.Dir = dir
-	//
-	// 	// setup buffer for stderr
-	// 	var stderr bytes.Buffer
-	// 	cmd.Stderr = &stderr
-	//
-	// 	out, err := cmd.Output()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
+	log.Println(splitStr)
+	log.Println(string(out), "err", err)
 	return []string{}
 }
